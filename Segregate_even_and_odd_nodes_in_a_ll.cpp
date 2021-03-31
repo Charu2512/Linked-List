4c4fe8d47b677ba4401cf1789ebc87e1276f0532
Charu2512 <64832473+Charu2512@users.noreply.github.com>
2021-04-01 00:24:27 +0530

    Create Segregate_even_and_odd_nodes_in_a_ll.cpp

---
+++ Segregate_even_and_odd_nodes_in_a_ll.cpp
@@ -0,0 +1,124 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+        if(head==NULL || head->next==NULL)
+        return head;
+        Node* evenh=NULL;
+        Node* event=NULL;
+        Node* oddh=NULL;
+        Node* oddt=NULL;
+        Node* temp=head;
+        while(temp)
+        {
+            if(temp->data %2==0)
+            {
+                if(evenh==NULL)
+                {
+                    evenh=temp;
+                    event=temp;
+                }
+                else
+                {
+                    event->next=temp;
+                    event=temp;
+                }
+            }
+            if(temp->data %2!=0)
+            {
+                if(oddh==NULL)
+                {
+                    oddh=temp;
+                    oddt=temp;
+                }
+                else
+                {
+                    oddt->next=temp;
+                    oddt=temp;
+                }
+            }
+            temp=temp->next;
+                
+            }
+            if(evenh==NULL)
+            return oddh;
+            else{
+                if(oddh==NULL)
+                return evenh;
+                else{
+            event->next=NULL;
+            oddt->next=NULL;
+            event->next=oddh;
+            return evenh;}
+            }
+        
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+  // } Driver Code Ends
